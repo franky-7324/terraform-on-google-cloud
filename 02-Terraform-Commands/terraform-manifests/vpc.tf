5421d6cd562d39f4208c67b472314990aa261d84
franky-7324 <anujdubey7324@gmail.com>
2025-02-28 08:37:45 +0530

    c3- updated the terraform and provider configuration

--- 02-Terraform-Commands/terraform-manifests/vpc.tf
+++ 02-Terraform-Commands/terraform-manifests/vpc.tf
@@ -1,7 +1,8 @@
 # Terraform Provider Configuration: google
 provider "google" {
-  project = "gcplearn9"
-  region = "us-central1"
+  project = "project-ai-ml-434700"
+  region = "asia-south2"
+
 }
 
 # Resource: VPC
@@ -13,8 +14,7 @@ resource "google_compute_network" "myvpc" {
 # Resource: Subnet
 resource "google_compute_subnetwork" "mysubnet" {
   name          = "subnet1"
-  region        = "us-central1"
+  region        = "asia-south2"
   ip_cidr_range = "10.128.0.0/20"
   network       = google_compute_network.myvpc.id  // GET VPC ID
 }
-
--- 03-Terraform-Language-Basics/terraform-manifests/c1-versions.tf
+++ 03-Terraform-Language-Basics/terraform-manifests/c1-versions.tf
@@ -1,17 +1,31 @@
 # Terraform Settings Block
+# terraform {
+#   required_version = ">= 1.8.5"
+#   required_providers {
+#     google = {
+#       source = "hashicorp/google"
+#       version = ">= 5.33.0"
+#     }
+#   }
+# }
+
+# # Terraform Provider Block
+# provider "google" {
+#   project = "gcplearn9" # PROJECT_ID
+#   region = "us-central1"
+# }
+
 terraform {
-  required_version = ">= 1.8.5"
+  required_version = ">=1.10.5"
   required_providers {
     google = {
       source = "hashicorp/google"
-      version = ">= 5.33.0"
+      version = "6.23.0"
     }
   }
 }
 
-# Terraform Provider Block
 provider "google" {
-  project = "gcplearn9" # PROJECT_ID
-  region = "us-central1"
+  project = "project-ai-ml-434700"
+  region = "asia-south2"
 }
-
